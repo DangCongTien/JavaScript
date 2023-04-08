09c7b67c620992ca81532b5a7454a67cdf6a687d
DangCongTien <dct12.1lvl@gmail.com>
2023-04-09 00:26:23 +0700

    Upload by Tien

---
+++ digitic-website-master/src/components/Footer.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { BsLinkedin, BsGithub, BsYoutube, BsInstagram } from "react-icons/bs";
+import newsletter from "../images/newsletter.png";
+const Footer = () => {
+  return (
+    <>
+      <footer className="py-4">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-4">
+              <h4 className="text-white mb-4">Contact Us</h4>
+              <div>
+                <address className="text-white fs-6">
+                  HCM City University of Technology
+                </address>
+                <a
+                  href="tel:+84 939333333"
+                  className="mt-3 d-block mb-1 text-white"
+                >
+                  +84 939333333
+                </a>
+                <a
+                  href="mailto:example@gmail.com"
+                  className="mt-2 d-block mb-0 text-white"
+                >
+                  example@gmail.com
+                </a>
+                <div className="social_icons d-flex align-items-center gap-30 mt-4">
+                  <a className="text-white" href="#">
+                    <BsLinkedin className="fs-4" />
+                  </a>
+                  <a className="text-white" href="#">
+                    <BsInstagram className="fs-4" />
+                  </a>
+                  <a className="text-white" href="#">
+                    <BsGithub className="fs-4" />
+                  </a>
+                  <a className="text-white" href="#">
+                    <BsYoutube className="fs-4" />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <footer className="py-4">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <p className="text-center mb-0 text-white">
+                &copy; {new Date().getFullYear()}; Powered by DCTien
+              </p>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
